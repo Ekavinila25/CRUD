5d1636c96a787bce11989499b3ff4a4d249a2603
Poetic Moon <kavinila@thelaunchclub.in>
2023-01-12 09:23:33 +0530

    messsage

--- contact/src/main/java/com/tlc/crm/contact/action/ContactListAction.java
+++ contact/src/main/java/com/tlc/crm/contact/action/ContactListAction.java
@@ -3,12 +3,6 @@ package com.tlc.crm.contact.action;
 import com.tlc.crm.common.action.secure.list.CrmListAction;
 import com.tlc.web.WebAction;
 
-/**
- * @author Abishek
- * @version 1.0
- */
-@WebAction(path = "/contact/list")
-public class ContactListAction extends CrmListAction
-{
+
 
 }
